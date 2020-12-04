e0cf274ba2653e2fe286dd1abd415f9aa97d9acb
WrnSz <verona@MacBook-Air-Weronika-2.local>
2020-12-04 16:26:55 +0100

    changed css

--- src/app.js
+++ src/app.js
@@ -134,8 +134,10 @@ function retrievePosition(position) {
   let lon = position.coords.longitude;
   let apiKey = "41d36454e5f3ee2db06d8969a9b290b7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
